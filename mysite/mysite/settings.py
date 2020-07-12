6c7c04606ef3092c5645a8924469ffc637d9d70d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-12 19:32:55 +0300

    Add icon

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,7 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Templates
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+# Static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
