ed152537ccbc3f1ce5579b3c48712fca439c05e5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-12 10:25:25 +0300

    Show information on HTML-page

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -7,8 +7,13 @@ load_dotenv()
 def index(request):
     appid = os.getenv('API_KEY')
     city = 'Kyiv'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
-    res = requests.get(url)
-    print(res.text)
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={appid}'
+    res = requests.get(url).json()
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+    }
+    context = {'info': city_info}
     template_name = 'weather/index.html'
-    return render(request, template_name)
+    return render(request, template_name, context)
