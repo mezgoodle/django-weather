d7c961be712b627ea1945e6ce569cd103f8bd016
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-12 11:39:21 +0300

    Registrate model

--- mysite/weather/admin.py
+++ mysite/weather/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import City
 
-# Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('id',)
+    search_fields = ['name',]
