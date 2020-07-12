da8e3f1d2a221fe01aeadb327560b9cd956aeb5d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-12 10:17:01 +0300

    Testing getting infor from API

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from dotenv import load_dotenv
+import os
+import requests
+load_dotenv()
 
 def index(request):
+    appid = os.getenv('API_KEY')
+    city = 'Kyiv'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
+    res = requests.get(url)
+    print(res.text)
     template_name = 'weather/index.html'
     return render(request, template_name)
