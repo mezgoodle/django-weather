308f9768adfb9d25992c4b47acaa5f919d80d78e
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-12 19:28:02 +0300

    Create unit-testing

--- mysite/weather/tests.py
+++ mysite/weather/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import City
 
-# Create your tests here.
+
+class CityModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        City.objects.create(name='Kyiv')
+
+    def test_name_content(self):
+        task = City.objects.get(id=1)
+        expected_object_name = f'{task.name}'
+        self.assertEquals(expected_object_name, 'Kyiv')
