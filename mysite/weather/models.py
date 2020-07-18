eb965eb8839563909848f8e8fce31b1d1ee90b9b
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-18 15:37:34 +0300

    Fix Codacy problems

--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -5,10 +5,12 @@ class City(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self) -> str:
+
         """Need for the admin panel and more"""
         return self.name
 
     class Meta:
+
         """Configuration"""
         ordering = ['-id']
         verbose_name = 'City'
